4e12c5da826aaf61006fecdb101c605bd17e9d3e
Micaela Scapino <micascapinomdq@gmail.com>
2021-07-13 20:30:28 -0300

    first steps

---
+++ 00-primeros-pasos/arrow-functions.js
@@ -0,0 +1,13 @@
+var plus = ( x, y ) => x + y; 
+var subtract = ( x, y ) => x - y; 
+var multiply = ( x, y ) => x * y; 
+var divide = ( x, y ) => x / y; 
+
+var x = 5;
+var y = 0;
+
+console.log({x, y});
+console.log("Addition:  " + plus(x,y));
+console.log("Subtraction:  " + subtract(x,y));
+console.log("Multiplication:  " + multiply(x,y));
+console.log("Division:  " + divide(x,y));
