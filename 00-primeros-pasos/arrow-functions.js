9efd58dabf0f4cd9c45b9976f4d36bc1e2072ae7
Micaela Scapino <micascapinomdq@gmail.com>
2021-07-13 21:18:54 -0300

    dding comments and new validations

--- 00-primeros-pasos/arrow-functions.js
+++ 00-primeros-pasos/arrow-functions.js
@@ -1,13 +1,46 @@
-var plus = ( x, y ) => x + y; 
-var subtract = ( x, y ) => x - y; 
-var multiply = ( x, y ) => x * y; 
-var divide = ( x, y ) => x / y; 
+//this function checks the variables are numbers and then adds them.
+var plus = ( x, y ) => {
+    if (typeof x === "number" && typeof y === "number")
+        return x + y; 
+    else
+        return("WARNING! the values must be numbers.");
+}
 
-var x = 5;
+//this function checks the variables are numbers and then subtract them.
+var subtract = ( x, y ) => {
+    if (typeof x === "number" && typeof y === "number")
+        return x - y; 
+    else
+        return("WARNING! the values must be numbers.");
+}
+
+//this function checks the variables are numbers and then multiply them.
+var multiply = ( x, y ) => {
+    if (typeof x === "number" && typeof y === "number")
+        return x * y;
+    else
+        return("WARNING! the values must be numbers.");
+}
+
+//this function divides two numbers unless the divider equals to zero.
+var divide = ( x, y ) =>
+{
+if (typeof x === "number" && typeof y === "number"){
+    if (y != 0)
+        return x / y
+    else
+        return("WARNING! You cannot divide for zero.");
+}
+else{
+    return("WARNING! The values must be numbers.");
+}
+}; 
+
+var x = 3;
 var y = 0;
 
 console.log({x, y});
-console.log("Addition:  " + plus(x,y));
-console.log("Subtraction:  " + subtract(x,y));
-console.log("Multiplication:  " + multiply(x,y));
-console.log("Division:  " + divide(x,y));
+console.log("Addition: " + plus(x,y));
+console.log("Subtraction: " + subtract(x,y));
+console.log("Multiplication: " + multiply(x,y));
+console.log("Division: " + divide(x,y));
