decabbfd785191d8d495cf06981d4cbff93b2804
Micaela Scapino <micascapinomdq@gmail.com>
2021-07-15 20:26:48 -0300

    new feature: results can be seen in files

--- 00-primeros-pasos/module2/app.js
+++ 00-primeros-pasos/module2/app.js
@@ -1,6 +1,23 @@
-var limit = 10;
-var number = 5;
+const fs = require('fs');
+
+//feel free to edit the numbers here below
+const limit = 10;
+const number = 5;
+
+//we will use the table to concatenate the results
+var table = "";
 
 for (let i = 1; i < limit+1; i++){
-    console.log(`${number} x ${i} = ${i*number}`);
+    table+=(`${number} x ${i} = ${i*number} \n`);
 }
+
+//we will replace the format of the file name according to the number choosen
+const fileName = 'table-'+ number +'.txt';
+//the writefile method has a callback to catch errors
+fs.writeFile(fileName,table,error =>{
+    if (error)
+        console.log(error)
+    else
+        console.log("File created successfully :)");
+        //shows this message when the file is created without errors.
+});
