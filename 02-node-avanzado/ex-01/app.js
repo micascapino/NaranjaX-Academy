e73f06c6a526d04ac15d7d3eb425add9d3e622b2
Micaela Scapino <micascapinomdq@gmail.com>
2021-07-22 19:54:43 -0300

    fix

--- 02-node-avanzado/ex-01/app.js
+++ 02-node-avanzado/ex-01/app.js
@@ -3,13 +3,22 @@ var array = [];
 
 //complete array with [0,1,2,3,4]
 for (let i=0; i<5;i++){
-    completeArray(i,add);
+    addNumber(i,showArray);
 }  
 
-function completeArray(number,callback) {
-    callback(number);
+function addNumber(number,callback) {
+    if (isNaN(number))
+        throw new Error("Must add a number.");
+    else{
+        array.push(number);
+        callback();
+    }
+
 }
 
-function add(number) {
-    array.push(number);
+function showArray(number,callback) {
+    if (array.length === 0)
+        throw new Error("This array is empty");
+    else
+        console.log(array);
 }
