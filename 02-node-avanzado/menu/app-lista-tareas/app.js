39e5423ed85e3f0cc87f613eafa00cae48b672df
Micaela Scapino <micascapinomdq@gmail.com>
2021-07-26 19:45:19 -0300

    fixed menu

--- 02-node-avanzado/menu/app-lista-tareas/app.js
+++ 02-node-avanzado/menu/app-lista-tareas/app.js
@@ -1,14 +1,14 @@
 const { showMenu, pause } = require("../helpers/messages");
 var colors = require('colors');
 
-console.clear();
 const main = async() => {
     let op= "";
     do{
+        console.clear();
         op = await showMenu();
         await pause();
 
-    }   while (op !== 0)
+    }   while (op !== '0');
 }
 
 main();
--- 02-node-avanzado/menu/helpers/messages.js
+++ 02-node-avanzado/menu/helpers/messages.js
@@ -1,10 +1,5 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 const showMenu = () =>{
     return new Promise((resolve,reject) => {
         console.log("======================".green);
@@ -19,18 +14,29 @@ const showMenu = () =>{
         console.log("6.".green + " Delete task");
         console.log("0.".green + " Exit");
 
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
         rl.question("Select an option: ", (op) =>{
-            if (op >= 1 || op <=6)
+            if (op >= 0 && op <=6)
                 resolve(op);
             else
-                reject("The option must be betweet 1 and 6.");
+                reject("The option must be betweet 0 and 6.".red);
             rl.close();
         })
     })
 }
 
 const pause = () =>{
-    return new Promise((resolve,reject) =>{
+    return new Promise((resolve) =>{
+            
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
         rl.question("Press ENTER to continue.",() =>{
             rl.close();
             resolve();
